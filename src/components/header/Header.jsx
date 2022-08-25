135bdf716282a0d668b9ca1907d23e608a081ca9
pwrq <pwrdpwnz@gmail.com>
2022-08-25 16:28:18 +0300

    add project to git

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,84 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-// import logo from './../images/logo.svg';
-// import user from './../images/user.svg';
-// import baskets from './../images/basket.svg';
-// import rel2 from './../images/rel-2png.png';
-// import palm from './../images/palm.png'
-// import iconEco from './../images/icon-eco.png'
+import s from './header.module.scss';
+
+import logo from './images/logo.svg';
+import titleImg from './images/header_img.png';
+import palm from './images/palm.png';
+import eco from './images/icon-eco.png'
+import meat from './images/myaso.png'
+
+import Menu from './menu/Menu';
 
 const Header = () => {
-  return (
-      // <header className="header">
-      //   <div className="rel">
-      //     <img className="rel-2" src={rel2} alt=""/>
-      //   </div>
-      //   <div className="container">
-      //     <nav className="navbar navbar-light navbar-expand-md py-3">
-      //       <div className="container">
-      //         <a className="navbar-brand d-flex align-items-center" href="#">
-      //           <img src={logo} alt=""/>
-      //         </a>
-      //         <button className="navbar-toggler" data-bs-toggle="collapse"
-      //                 data-bs-target="#navcol-4"><span
-      //             className="visually-hidden">Toggle navigation</span><span
-      //             className="navbar-toggler-icon"></span></button>
-      //         <div id="navcol-4"
-      //              className="collapse navbar-collapse flex-grow-0 order-md-first">
-      //           <ul className="navbar-nav me-auto">
-      //             <li className="nav-item">
-      //               <a className="nav-link active" href="#">ПРОДУКЦІЯ</a>
-      //             </li>
-      //             <li className="nav-item">
-      //               <a className="nav-link" href="#">Рецепти</a>
-      //             </li>
-      //             <li className="nav-item">
-      //               <a className="nav-link" href="#">вiдповiдi</a>
-      //             </li>
-      //             <li className="nav-item">
-      //               <a className="nav-link" href="#">вiдгуки</a>
-      //             </li>
-      //           </ul>
-      //           <div className="d-md-none my-2">
-      //             <a className="btn btn-primary " role="button"
-      //                href="tel:380502949360">+38 (050)-294-93-60</a>
-      //           </div>
-      //         </div>
-      //         <div className="d-none d-md-block">
-      //           <ul className="navbar-nav me-auto">
-      //             <li className="nav-item mr">
-      //               <a className="nav-link active"
-      //                  href="#">ПРО нас</a></li>
-      //             <li className="mr100">
-      //               <a className="btn btn-primary" role="button"
-      //                  href="tel:380502949360">+38 (050)-294-93-60</a>
-      //             </li>
-      //             <li className="nav-item mr">
-      //               <a className="nav-link active"
-      //                  href="#">
-      //                 <img src={user} alt=""/>
-      //               </a></li>
-      //             <li className="nav-item mr">
-      //               <a className="nav-link active"
-      //                  href="#">
-      //                 <img src={baskets} alt=""/>
-      //               </a>
-      //             </li>
-      //           </ul>
-      //         </div>
-      //       </div>
-      //     </nav>
-      //   </div>
-      //   <div className="container">
-      //     <div className="header_title">
-      //       <img className='palm' src={palm} alt=""/>
-      //       ДОМАШНІ <img className='iconEco' src={iconEco} alt=""/><span className='span'>ПРОДУКТИ</span><br />
-      //       ВІД Ципи
-      //     </div>
-      //   </div>
-      // </header>
-      <header className="header">
+  const [menuActive, setMenuActive] = useState(false);
+  const items = [
 
+    {value: 'Главная', href: '/main', icons: 'forward'},
+    {value: 'Услуги', href: '/service', icons: 'forward'},
+    {value: 'Услуги', href: '/service', icons: 'forward'},
+    {value: 'Услуги', href: '/service', icons: 'forward'},
+    {value: 'Магазин', href: '/magazine', icons: 'forward'},
+    {value: 'Магазин', href: '/magazine', icons: 'forward'},
+    {value: 'Магазин', href: '/magazine', icons: 'forward'},
+    {value: 'Магазин', href: '/magazine', icons: 'forward'},
+
+  ];
+  return (
+      <header className={s.header}>
+        <div className="container">
+          <nav className={s.nav}>
+            <img src={logo} alt="logo" className={s.logo}/>
+            <div className={s.burgerBtn}
+                 onClick={() => setMenuActive(!menuActive)}>
+              <span/>
+            </div>
+          </nav>
+        </div>
+        <Menu active={menuActive} setActive={setMenuActive} header={'Menu'}
+              items={items}/>
+        <div className={s.title}>
+          <div className={s.titleContainer}>
+            <img className={s.title_img} src={titleImg} alt=""/>
+            <img src={palm} alt="" className={s.palm}/>
+            <img src={eco} alt="" className={s.eco}/>
+            <p>З ДОСТАВКОЮ ПО ПОЛТАВI</p>
+          </div>
+        </div>
+        <div className={s.subTitle}>
+          <img className={s.subTitle_img}  src={meat} alt=""/>
+          <p className={s.subTitle_ph}>Всіх раді вітати на сторінках сайту нашої міні-ферми!</p>
+        </div>
       </header>
   );
 };
---
+++ src/components/header/menu/Menu.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './menu.css'
+
+const Menu = ({header, items, active, setActive}) => {
+  return (
+      <div className={active ? 'menu active' : 'menu'} onClick={() => setActive(false)}>
+        <div className="blur"/>
+        <div className="menu__content" onClick={e => e.stopPropagation()}>
+          <div className="menu__header">{header}</div>
+          <ul>
+            {items.map(item =>
+                    <li>
+                      <a href={item.href}>{item.value}</a>
+                      <span className="material-symbols-outlined">{item.icon}</span>
+                    </li>
+            )}
+          </ul>
+        </div>
+      </div>
+  );
+};
+
+export default Menu;
