a5c6f1d8c8e0bacb5ea21b24db3be92c87c96007
pwrq <pwrdpwnz@gmail.com>
2022-08-25 16:27:06 +0300

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+
+// import logo from './../images/logo.svg';
+// import user from './../images/user.svg';
+// import baskets from './../images/basket.svg';
+// import rel2 from './../images/rel-2png.png';
+// import palm from './../images/palm.png'
+// import iconEco from './../images/icon-eco.png'
+
+const Header = () => {
+  return (
+      // <header className="header">
+      //   <div className="rel">
+      //     <img className="rel-2" src={rel2} alt=""/>
+      //   </div>
+      //   <div className="container">
+      //     <nav className="navbar navbar-light navbar-expand-md py-3">
+      //       <div className="container">
+      //         <a className="navbar-brand d-flex align-items-center" href="#">
+      //           <img src={logo} alt=""/>
+      //         </a>
+      //         <button className="navbar-toggler" data-bs-toggle="collapse"
+      //                 data-bs-target="#navcol-4"><span
+      //             className="visually-hidden">Toggle navigation</span><span
+      //             className="navbar-toggler-icon"></span></button>
+      //         <div id="navcol-4"
+      //              className="collapse navbar-collapse flex-grow-0 order-md-first">
+      //           <ul className="navbar-nav me-auto">
+      //             <li className="nav-item">
+      //               <a className="nav-link active" href="#">ПРОДУКЦІЯ</a>
+      //             </li>
+      //             <li className="nav-item">
+      //               <a className="nav-link" href="#">Рецепти</a>
+      //             </li>
+      //             <li className="nav-item">
+      //               <a className="nav-link" href="#">вiдповiдi</a>
+      //             </li>
+      //             <li className="nav-item">
+      //               <a className="nav-link" href="#">вiдгуки</a>
+      //             </li>
+      //           </ul>
+      //           <div className="d-md-none my-2">
+      //             <a className="btn btn-primary " role="button"
+      //                href="tel:380502949360">+38 (050)-294-93-60</a>
+      //           </div>
+      //         </div>
+      //         <div className="d-none d-md-block">
+      //           <ul className="navbar-nav me-auto">
+      //             <li className="nav-item mr">
+      //               <a className="nav-link active"
+      //                  href="#">ПРО нас</a></li>
+      //             <li className="mr100">
+      //               <a className="btn btn-primary" role="button"
+      //                  href="tel:380502949360">+38 (050)-294-93-60</a>
+      //             </li>
+      //             <li className="nav-item mr">
+      //               <a className="nav-link active"
+      //                  href="#">
+      //                 <img src={user} alt=""/>
+      //               </a></li>
+      //             <li className="nav-item mr">
+      //               <a className="nav-link active"
+      //                  href="#">
+      //                 <img src={baskets} alt=""/>
+      //               </a>
+      //             </li>
+      //           </ul>
+      //         </div>
+      //       </div>
+      //     </nav>
+      //   </div>
+      //   <div className="container">
+      //     <div className="header_title">
+      //       <img className='palm' src={palm} alt=""/>
+      //       ДОМАШНІ <img className='iconEco' src={iconEco} alt=""/><span className='span'>ПРОДУКТИ</span><br />
+      //       ВІД Ципи
+      //     </div>
+      //   </div>
+      // </header>
+      <header className="header">
+
+      </header>
+  );
+};
+export default Header;
